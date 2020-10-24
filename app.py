b15b976f34d8e84eaec2a5fd0c317126b24cdd17
Rainer Brieger <hello@rainerb.com>
2020-10-24 17:10:05 -0600

    Fixed function logic

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # FizzBuzz
+numbers = [15, 15, 27, 30, 29, 13, 21, 1, 2, 20, 18, 5, 19, 9, 11, 22, 15, 23, 0, 6, 12, 7, 17, 14, 16, 4, 28, 3, 8, 10]
 
-for number in range(1, 30):
-  if(number % 3):
-    print("Fizz")
-  elif(number % 5):
-    print("Buzz")
-  elif(number % 3 and number % 5 ):
-    print("FizzBuzz")
+def fizzBuzz(number):
+    if(number % 3 == 0  and number % 5 == 0):
+      print("FizzBuzz")
+    elif(number % 5 == 0):
+      print("Buzz")
+    elif(number % 3 == 0):
+      print("Fizz")
+
+for number in numbers:
+  fizzBuzz(number)
